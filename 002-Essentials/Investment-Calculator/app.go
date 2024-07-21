445530ec96aa101a0446a8c4c730540ab32d293e
Nick Rabone <n.rabone72@aol.com>
2024-07-21 05:16:32 +0100

    Started small mini project in Go - learning about data types.

---
+++ 002-Essentials/Investment-Calculator/app.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var investmentAmount = 10000
+	var expectedReturnRate = 6.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + int(expectedReturnRate) /  100)
+}
---
+++ 002-Essentials/Investment-Calculator/go.mod
@@ -0,0 +1,3 @@
+module nimiratech.com/inv-calc
+
+go 1.22.5
